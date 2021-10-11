526ae289d2ed5c78a80b7329f85058d6ead726d6
dianepj12@gmail.com <dianepj12@gmail.com>
2021-10-11 18:14:43 -0500

    pop up cocktail button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,138 +1,150 @@
+//button
+//onclick does no appy to multiple style but can not be used in all browsers
+// document.querySelector("#dinner-drinks").addEventListener("click", function (){
+//     console.log("Does something");
+// })
+//document.querySelector("#dinner-drinks").addEventListener("click", function (){
+  //  document.querySelector("#splashy").textContent="Does something";
+//})
 
 // Random cocktail variables
 var randomCocktail = document.querySelector("#random-cocktail");
 var drinkCategory = document.querySelector("#drink-category");
 var glassType = document.querySelector ("#glass-type");
-// var ingredient1 = document.createElement("p");
-// var ingredient2 = document.createElement("p");
-// var ingredient3 = document.createElement("p");
-// var ingredient4 = document.createElement("p");
-// var ingredient5 = document.createElement("p");
-// var ingredient6 = document.createElement("p");
-// var ingredient7 = document.createElement("p");
+  
+
+//for (let i = 0 ; i<7; i++){
+ var ingredient1 = document.createElement("p");
+ var ingredient2 = document.createElement("p");
+ var ingredient3 = document.createElement("p");
+var ingredient4 = document.createElement("p");
+ var ingredient5 = document.createElement("p");
+var ingredient6 = document.createElement("p");
+var ingredient7 = document.createElement("p");
+
 
 // Random meal variables
-var randomMeal = document.querySelector("#random-meal");
-var mealCategory = document.querySelector("#meal-category");
-var cuisine = document.querySelector("#cuisine");
-// var mealIngredient1 = document.createElement("p");
-// var mealIngredient2 = document.createElement("p");
-// var mealIngredient3 = document.createElement("p");
-// var mealIngredient4 = document.createElement("p");
-// var mealIngredient5 = document.createElement("p");
-// var mealIngredient6 = document.createElement("p");
-// var mealIngredient7 = document.createElement("p");
+    var randomMeal = document.querySelector("#random-meal");
+     var mealCategory = document.querySelector("#meal-category");
+    var cuisine = document.querySelector("#cuisine");
+    var mealIngredient1 = document.createElement("p");
+     var mealIngredient2 = document.createElement("p");
+    var mealIngredient4 = document.createElement("p");
+    var mealIngredient5 = document.createElement("p");
+    var mealIngredient6 = document.createElement("p");
+    var mealIngredient7 = document.createElement("p");
 
 
 
-// Generate random cocktail
+// // Generate random cocktail
 function cocktail() {
-fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
 
 .then(function(response) {
     return response.json();
-})
-.then(function(data) {
-    console.log(data);
+ })
+ .then(function(data) {
+     console.log(data);
 
 
-    // document.body.append(ingredient1);
-    // document.body.append(ingredient2);
-    // document.body.append(ingredient3);
-    // document.body.append(ingredient4);
-    // document.body.append(ingredient5);
-    // document.body.append(ingredient6);
-    // document.body.append(ingredient7);
+    document.body.append(ingredient1);
+    document.body.append(ingredient2);
+    document.body.append(ingredient3);
+    document.body.append(ingredient4);
+    document.body.append(ingredient5);
+    document.body.append(ingredient6);
+    document.body.append(ingredient7);
 
 
     var randomCocktailValue = data.drinks[0].strDrink;
-    // var alcoholicValue = data.drinks[0].strAlcoholic;
+    var alcoholicValue = data.drinks[0].strAlcoholic;
     var drinkCategoryValue = data.drinks[0].strCategory;
     var glassTypeValue = data.drinks[0].strGlass;
-    // var ingredient1Value = data.drinks[0].strIngredient1;
-    // var ingredient2Value = data.drinks[0].strIngredient2;
-    // var ingredient3Value = data.drinks[0].strIngredient3;
-    // var ingredient4Value = data.drinks[0].strIngredient4;
-    // var ingredient5Value = data.drinks[0].strIngredient5;
-    // var ingredient6Value = data.drinks[0].strIngredient6;
-    // var ingredient7Value = data.drinks[0].strIngredient7;
+    var ingredient1Value = data.drinks[0].strIngredient1;
+    var ingredient2Value = data.drinks[0].strIngredient2;
+    var ingredient3Value = data.drinks[0].strIngredient3;
+    var ingredient4Value = data.drinks[0].strIngredient4;
+    var ingredient5Value = data.drinks[0].strIngredient5;
+    var ingredient6Value = data.drinks[0].strIngredient6;
+    var ingredient7Value = data.drinks[0].strIngredient7;
 
-    randomCocktail.innerHTML = randomCocktailValue;
-    // alcoholic.innerHTML = alcoholicValue;
-    drinkCategory.innerHTML = drinkCategoryValue;
-    glassType.innerHTML = glassTypeValue;
-    // ingredient1.innerHTML = ingredient1Value;
-    // ingredient2.innerHTML = ingredient2Value;
-    // ingredient3.innerHTML = ingredient3Value;
-    // ingredient4.innerHTML = ingredient4Value;
-    // ingredient5.innerHTML = ingredient5Value;
-    // ingredient6.innerHTML = ingredient6Value;
-    // ingredient7.innerHTML = ingredient7Value;
+     randomCocktail.innerHTML = randomCocktailValue;
+     alcoholic.innerHTML = alcoholicValue;
+     drinkCategory.innerHTML = drinkCategoryValue;
+     glassType.innerHTML = glassTypeValue;
+     ingredient1.innerHTML = ingredient1Value;
+     ingredient2.innerHTML = ingredient2Value;
+     ingredient3.innerHTML = ingredient3Value;
+     ingredient4.innerHTML = ingredient4Value;
+     ingredient5.innerHTML = ingredient5Value;
+     ingredient6.innerHTML = ingredient6Value;
+     ingredient7.innerHTML = ingredient7Value;
 
 
+ })
+    .catch(function (err){
+     console.log(err);
+ })
+ }
+ //cocktail();
+ document.querySelector("#dinner-drinks").addEventListener("click", function (){
+    document.querySelector("#splashy").textContent=cocktail();
+ 
+    
 })
-.catch(function (err){
-    console.log(err);
-})
 
-}
-cocktail();
+ 
 
 // Generate random meal
 function meal() {
-    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+   fetch("https://www.themealdb.com/api/json/v1/1/random.php")
 
     .then(function(response) {
-        return response.json();
-    })
+         return response.json();
+     })
     .then(function(data) {
         console.log(data);
 
-        // document.body.append(mealIngredient1);
-        // document.body.append(mealIngredient2);
-        // document.body.append(mealIngredient3);
-        // document.body.append(mealIngredient4);
-        // document.body.append(mealIngredient5);
-        // document.body.append(mealIngredient6);
-        // document.body.append(mealIngredient7);
+         document.body.append(mealIngredient1);
+         document.body.append(mealIngredient2);
+         document.body.append(mealIngredient3);
+         document.body.append(mealIngredient4);
+         document.body.append(mealIngredient5);
+         document.body.append(mealIngredient6);
+         document.body.append(mealIngredient7);
 
 
-        var randomMealValue = data.meals[0].strMeal;
-        var mealCategoryValue = data.meals[0].strCategory;
-        var cuisineValue = data.meals[0].strArea;
-        // var mealIngredient1Value = data.meals[0].strIngredient1;
-        // var mealIngredient2Value = data.meals[0].strIngredient2;
-        // var mealIngredient3Value = data.meals[0].strIngredient3;
-        // var mealIngredient4Value = data.meals[0].strIngredient4;
-        // var mealIngredient5Value = data.meals[0].strIngredient5;
-        // var mealIngredient6Value = data.meals[0].strIngredient6;
-        // var mealIngredient7Value = data.meals[0].strIngredient7;
+          var randomMealValue = data.meals[0].strMeal;
+          var mealCategoryValue = data.meals[0].strCategory;
+          var cuisineValue = data.meals[0].strArea;
+          var mealIngredient1Value = data.meals[0].strIngredient1;
+          var mealIngredient2Value = data.meals[0].strIngredient2;
+          var mealIngredient3Value = data.meals[0].strIngredient3;
+          var mealIngredient4Value = data.meals[0].strIngredient4;
+          var mealIngredient5Value = data.meals[0].strIngredient5;
+          var mealIngredient7Value = data.meals[0].strIngredient7;
 
 
-        randomMeal.innerHTML = randomMealValue;
-        mealCategory.innerHTML = mealCategoryValue;
-        cuisine.innerHTML = cuisineValue;
-        // mealIngredient1.innerHTML = mealIngredient1Value;
-        // mealIngredient2.innerHTML = mealIngredient2Value;
-        // mealIngredient3.innerHTML = mealIngredient3Value;
-        // mealIngredient4.innerHTML = mealIngredient4Value;
-        // mealIngredient5.innerHTML = mealIngredient5Value;
-        // mealIngredient6.innerHTML = mealIngredient6Value;
-        // mealIngredient7.innerHTML = mealIngredient7Value;
-    })
-    .catch(function (err){
-        console.log(err);
-    })
+         randomMeal.innerHTML = randomMealValue;
+         mealCategory.innerHTML = mealCategoryValue;
+         cuisine.innerHTML = cuisineValue;
+         mealIngredient1.innerHTML = mealIngredient1Value;
+         mealIngredient2.innerHTML = mealIngredient2Value;
+         mealIngredient3.innerHTML = mealIngredient3Value;
+         mealIngredient4.innerHTML = mealIngredient4Value;
+         mealIngredient5.innerHTML = mealIngredient5Value;
+         mealIngredient6.innerHTML = mealIngredient6Value;
+        mealIngredient7.innerHTML = mealIngredient7Value;
+          })
+      .catch(function (err){
+           console.log(err);
+   })
 
-    }
-    meal();
-// //button
-// function createButton(){
-//     var button = document.createElement("button");
-//     element.appendChild(document.createTextNode("Click Me!"));
-//     var page = document.getElementById("button");
-//     page.appendChild(element);
-//     console.log(element);
-//
-// }
-// createButton();
+  }
+   //meal();
+
+   document.querySelector("#dinner-drinks").addEventListener("click", function (){
+    document.querySelector("#mrecipe").textContent = meal();
+ 
+    
+})
